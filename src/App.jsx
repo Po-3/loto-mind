e8c509c6cc8835587a8fa90c5c8c530d3da0fb21
po-san <goolegleu@gmail.com>
2025-07-20 17:11:44 +0900

    update message

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const tabs = [
 
 // 機能タブ
 const features = [
-  { key: 'past', label: '過去検索' },
+  { key: 'past', label: '過去データ' },
   { key: 'diagnosis', label: 'となり診断' },
   { key: 'prediction', label: 'ズバリ予想' },
   { key: 'settings', label: '設定' }
