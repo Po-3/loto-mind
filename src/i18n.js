5881102110cc4d06d9dbaf679b6cf2a066e7f96a
po-san <goolegleu@gmail.com>
2025-07-21 22:50:39 +0900

    update message

--- src/i18n.js
+++ src/i18n.js
@@ -1,11 +1,10 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-// 作成したjsonをimport
-import ja from './ja.json';
-import en from './en.json';
-import zh from './zh.json';
-import zhTW from './zh-TW.json';
+import ja from './locales/ja.json';
+import en from './locales/en.json';
+import zh from './locales/zh.json';
+import zhTW from './locales/zh-TW.json';
 
 const resources = {
   ja: { translation: ja },
@@ -18,10 +17,11 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: navigator.language.startsWith('zh-TW') ? 'zh-TW'
+    lng:
+      navigator.language.startsWith('zh-TW') ? 'zh-TW'
       : navigator.language.startsWith('zh') ? 'zh'
       : navigator.language.startsWith('ja') ? 'ja'
-      : 'en', // デフォルト言語（自動判定）
+      : 'en',
     fallbackLng: 'ja',
     interpolation: { escapeValue: false },
   });
